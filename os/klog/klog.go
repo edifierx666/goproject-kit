cd73cb9d6397a4d4c85218d8bb9601ba088f76f3
zhangyu <11>
2022-09-01 20:49:23 +0800

    feat:初始化

--- os/klog/klog.go
+++ os/klog/klog.go
@@ -2,6 +2,9 @@ package klog
 
 import "go.uber.org/zap"
 
-func New(cfgs ...*EasyZapCfg) *zap.Logger {
+func New(easyZaps ...*EasyZap) *zap.Logger {
+  if len(easyZaps) > 0 {
+    return Zap(easyZaps[0])
+  }
   return Zap(NewEasyZap())
 }
