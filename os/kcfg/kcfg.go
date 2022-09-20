38ed500d97026630e536cab574ac331a670e1972
zhangyu <11>
2022-09-20 10:08:34 +0800

    feat:修改kcfg

--- os/kcfg/kcfg.go
+++ os/kcfg/kcfg.go
@@ -6,32 +6,28 @@ import (
 )
 
 type Kcfg struct {
-  viper      *viper.Viper
+  *viper.Viper
   Path       string `json:"path" yaml:"path"`
   MergeEnv   bool   `json:"merge_env" yaml:"merge_env"`
   ConfigType string `json:"config_type" yaml:"config_type"`
   WatchMode  bool   `json:"watch_mode" yaml:"watch_mode"`
 }
 
-func (k *Kcfg) Viper() *viper.Viper {
-  return k.viper
-}
-
 func (k *Kcfg) Build() *Kcfg {
   k.SetConfigPath(k.Path)
   k.SetConfigType(k.ConfigType)
   if k.MergeEnv {
-    k.viper.AutomaticEnv()
+    k.Viper.AutomaticEnv()
   }
   if k.WatchMode {
-    k.viper.WatchConfig()
+    k.Viper.WatchConfig()
   }
   return k
 }
 
 func New() *Kcfg {
   kcfg := &Kcfg{
-    viper:      viper.New(),
+    Viper:      viper.New(),
     MergeEnv:   false,
     ConfigType: "yaml",
     WatchMode:  true,
@@ -42,7 +38,7 @@ func New() *Kcfg {
 
 func (k *Kcfg) SetConfigPath(path string) *Kcfg {
   k.Path = path
-  k.viper.SetConfigFile(path)
+  k.Viper.SetConfigFile(path)
   return k
 }
 
@@ -51,26 +47,21 @@ func (k *Kcfg) SetConfigType(t string) *Kcfg {
     t = "yaml"
   }
   k.ConfigType = t
-  k.viper.SetConfigType(t)
+  k.Viper.SetConfigType(t)
   return k
 }
 
 func (k *Kcfg) OnConfigChange(fn func(e fsnotify.Event)) *Kcfg {
-  k.viper.OnConfigChange(fn)
+  k.Viper.OnConfigChange(fn)
   return k
 }
 
 func (k *Kcfg) Update() *Kcfg {
-  err := k.viper.ReadInConfig()
+  err := k.Viper.ReadInConfig()
   if err != nil {
     panic(err)
   }
   return k
-}
-
-func (k *Kcfg) Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error {
-  err := k.viper.Unmarshal(rawVal, opts...)
-  return err
 }
 
 func (k *Kcfg) ReadAsMap() (error, map[string]interface{}) {
